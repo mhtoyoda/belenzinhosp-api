8f66aab1d312c198fb6c5b1adaecaf49104fe47d
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2018-12-18 21:29:37 -0300

    Estrutura inicial API BelenzinhoSP

---
+++ src/main/java/br/com/belenzinhospapi/BelenzinhospApiApplication.java
@@ -0,0 +1,14 @@
+package br.com.belenzinhospapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelenzinhospApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelenzinhospApiApplication.class, args);
+	}
+
+}
+
