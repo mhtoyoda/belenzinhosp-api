f902cee266b547d369ee23faeee6c2a77f24f823
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-01-07 15:55:32 -0200

    Configuracao de api de envio de email

---
+++ src/main/java/br/com/belenzinhosp/model/Email.java
@@ -0,0 +1,32 @@
+package br.com.belenzinhosp.model;
+
+public class Email {
+
+    private String subject;
+    private String text;
+    private String email;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/br/com/belenzinhosp/service/EmailService.java
@@ -0,0 +1,35 @@
+package br.com.belenzinhosp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${email.to}")
+    private String emailTo;
+
+    public boolean sendEmail(String email, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setText("Email FROM: "+email +" - "+text);
+        message.setTo(emailTo);
+        message.setSubject(subject);
+        message.setSentDate(new Date());
+        message.setFrom("marcelo.toyoda@koin.com.br");
+        try {
+            mailSender.send(message);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
