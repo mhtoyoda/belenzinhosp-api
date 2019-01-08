529cd416bf3228949deade852f65f333a12751ed
Marcelo Toyoda <marcelo.toyoda@koin.com.br>
2019-01-08 16:50:55 -0200

    Api de envio de e-mail

---
+++ src/main/java/br/com/belenzinhosp/model/Email.java
@@ -0,0 +1,32 @@
+package br.com.belenzinhosp.model;
+
+public class Email {
+
+    private String subject;
+    private String message;
+    private String email;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/br/com/belenzinhosp/resource/EmailResource.java
@@ -0,0 +1,25 @@
+package br.com.belenzinhosp.resource;
+
+import br.com.belenzinhosp.model.Email;
+import br.com.belenzinhosp.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmailResource {
+
+    @Autowired
+    private EmailService emailService;
+
+    @PostMapping("/api/email")
+    public ResponseEntity<String> email(@RequestBody Email email){
+        boolean sentEmail = emailService.sendEmail(email.getEmail(), email.getSubject(), email.getMessage());
+        if(sentEmail){
+            return ResponseEntity.ok("OK");
+        }
+        return ResponseEntity.ok("NOK");
+    }
+}
